5b63d4384e4502eb8005a184bc0b0d81bbacbe7c
unknown <kjpds@mail.ru>
2025-01-13 13:46:16 +0300

    first commit

---
+++ blog/views.py
@@ -0,0 +1,139 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
+from django.contrib.postgres.search import TrigramSimilarity
+
+def post_share(request, post_id):
+    # Извлечь пост по идентификатору id
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    sent = False
+
+    if request.method == 'POST':
+        # Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+            # ... отправить электронное письмо
+            post_url = request.build_absolute_uri(post.get_absolute_url())   #это метод объекта request, который используется для создания абсолютного URI
+            subject = (f"{cd['name']} recommends you read "
+                       f"{post.title}") #формы
+            message = (f"Read {post.title} at {post_url}\n\n "
+                       f"{cd['name']}\'s ({cd['email']}) comments: {cd['comments']}")
+            send_mail(subject, message, settings.EMAIL_HOST_USER,
+                      [cd['to']])   #функция для отправки эл писем
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post,
+                                                    'form': form,
+                                                    'sent': sent})
+
+# class PostListView(ListView):
+#     """
+#     Альтернативное представление списка постов
+#     """
+#     queryset = Post.published.all()
+#     context_object_name = 'posts'
+#     paginate_by = 3
+#     template_name = 'blog/post/list.html'
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+
+    # Постраничная разбивка с 3 постами на страницу
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # Если page_number не целое число, то
+        # выдать первую страницу
+        posts = paginator.page(1)
+    except EmptyPage:
+        # Если page_number находится вне диапазона, то
+        # выдать последнюю страницу результатов
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                  'blog/post/list.html',
+                  {'posts': posts,
+                   'tag': tag})
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    # Список активных комментариев к этому посту
+    comments = post.comments.filter(active=True)
+
+    # Форма для комментариев пользователей
+    form = CommentForm()
+
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids) \
+        .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')) \
+                        .order_by('-same_tags', '-publish')[:4]
+    return render(request,
+                  'blog/post/detail.html',
+                  {'post': post,
+                   'comments': comments,
+                   'form': form,
+                   'similar_posts': similar_posts})
+@require_POST   #ограничивает доступ к представлению только POST-запросами
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    # Комментарий был отправлен
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Создать объект класса Comment, не сохраняя его в базе данных
+        comment = form.save(commit=False)
+        # Назначить пост комментарию
+        comment.post = post
+        # Сохранить комментарий в базе данных
+        comment.save()
+    return render(request, 'blog/post/comment.html',
+                  {'post': post,
+                   'form': form,
+                   'comment': comment})
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+
+    return render(request,
+                  'blog/post/search.html',
+                  {'form': form,
+                   'query': query,
+                   'results': results})
